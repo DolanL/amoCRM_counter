12ac82b2cb08ad5ac6c7c30b0e065c7193d32b12
DolanL <dlakshinov@inbox.ru>
2023-04-09 11:55:58 +0300

    added counter in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, useState, KeyboardEvent} from 'react';
 import './App.css';
 
 function App() {
+
+  const [value, setValue] = useState('')
+  const [counter, setCounter] = useState<number>(0)
+  const [timerId, setTimerId] = useState<number>(0)
+  let seconds = JSON.stringify(counter % 60);
+  let minutes = JSON.stringify(Math.floor(counter / 60) % 60);
+  let hours = JSON.stringify(Math.floor(counter / 3600));
+
+  seconds = +seconds < 10 ? '0' + Math.floor(counter % 60) : JSON.stringify(Math.floor(counter % 60));
+  minutes = +minutes < 10 ? '0' + Math.floor(counter / 60) % 60 : JSON.stringify(Math.floor(counter / 60) % 60);
+  hours = +hours < 10 ? '0' + Math.floor(counter / 3600) : JSON.stringify(Math.floor(counter / 3600));
+
+
+  const onCLickHandler = () => {
+    setCounter(+value)
+    const idInterval: number = window.setInterval(() => {
+      setCounter(counter => counter - 1)
+    }, 1000)
+    setTimerId(idInterval)
+  }
+
+  const onKeyDownHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      onCLickHandler()
+    }
+  }
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.currentTarget.value)
+  }
+
+
+  if (counter <= 0) {
+    clearInterval(timerId)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={onChangeHandler} onKeyDown={onKeyDownHandler} value={value} type="number" placeholder='seconds'/>
+      <button onClick={onCLickHandler}>start</button>
+      <p>{`${hours}:${minutes}:${seconds}`}</p>
+      <p></p>
     </div>
   );
 }
